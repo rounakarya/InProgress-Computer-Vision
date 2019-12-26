cbea33f2f355ecaa487bd7fa51761ef3f304ac4c
Rounak Arya <rounakarya30@gmail.com>
2019-12-26 18:45:52 +0530

    Updated and Added multiple files(shallownet)

--- shallownet_cifar10.py
+++ shallownet_cifar10.py
@@ -30,7 +30,7 @@ model.compile(loss="categorical_crossentropy", optimizer=opt, metrics=["accuracy
 
 # train the network
 print("[INFO] training network...")
-H = model.fit(trainX, trainY, validation_data=(testX, testY), batch_size=32, epochs=100, verbose=1)
+H = model.fit(trainX, trainY, validation_data=(testX, testY), batch_size=32, epochs=40, verbose=1)
 
 # evaluate the network
 print("[INFO] evaluating network...")
@@ -40,10 +40,10 @@ print(classification_report(testY.argmax(axis=1), predictions.argmax(axis=1), ta
 # plot the training loss and accuracy
 plt.style.use("ggplot")
 plt.figure()
-plt.plot(np.arange(0, 100), H.history["loss"], label="train_loss")
-plt.plot(np.arange(0, 100), H.history["val_loss"], label="val_loss")
-plt.plot(np.arange(0, 100), H.history["acc"], label="train_acc")
-plt.plot(np.arange(0, 100), H.history["val_acc"], label="val_acc")
+plt.plot(np.arange(0, 40), H.history["loss"], label="train_loss")
+plt.plot(np.arange(0, 40), H.history["val_loss"], label="val_loss")
+plt.plot(np.arange(0, 40), H.history["acc"], label="train_acc")
+plt.plot(np.arange(0, 40), H.history["val_acc"], label="val_acc")
 plt.title("Training Loss and Accuracy")
 plt.xlabel("Epoch #")
 plt.ylabel("Loss/Accuracy")
